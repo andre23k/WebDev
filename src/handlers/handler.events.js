6d4781fb4e3da15b93e35642130828a3f27cdc76
André <102836717+andre23k@users.noreply.github.com>
2024-01-26 23:29:26 -0300

    add classes

--- src/handlers/handler.events.js
+++ src/handlers/handler.events.js
@@ -2,6 +2,7 @@ import { readdirSync } from 'fs'
 
 const eventsFilesNames = readdirSync('src/events/').filter(fileName => fileName.endsWith('.js'))
 const eventsFilesFunctions = readdirSync('src/events/functions/').filter(fileName => fileName.endsWith('.js'))
+const eventsFilesclasses = readdirSync('src/events/classes/').filter(fileName => fileName.endsWith('.js'))
 
 for (const eventFileName of eventsFilesFunctions){
   import(`../events/functions/${eventFileName}`)
@@ -9,5 +10,8 @@ for (const eventFileName of eventsFilesFunctions){
 for (const eventFileName of eventsFilesNames) {
   import(`../events/${eventFileName}`)
 }
+for (const eventFileName of eventsFilesclasses) {
+  import(`../events/classes/${eventFileName}`)
+}
 
-console.log(`${[...eventsFilesNames, ...eventsFilesFunctions].length} Events | OK`)
+console.log(`${[...eventsFilesNames, ...eventsFilesFunctions, ...eventsFilesclasses ].length} Events | OK`)
