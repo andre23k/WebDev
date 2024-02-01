93c5d19c972ee077b0e3358c67e905cc60d61226
André <102836717+andre23k@users.noreply.github.com>
2024-02-01 19:27:58 -0300

    .

--- src/handlers/handler.events.js
+++ src/handlers/handler.events.js
@@ -1,5 +1,11 @@
 import { readdirSync } from 'fs'
 const eventsFilesNames = readdirSync('./src/events/').filter(fileName => fileName.endsWith('.js'))
+const eventsFilesFunctions = readdirSync('src/events/functions/').filter(fileName => fileName.endsWith('.js'))
+
 for (const eventFileName of eventsFilesNames)
   import(`../events/${eventFileName}`)
-console.log(`${[...eventsFilesNames].length} Events | OK`)
+
+for (const eventFileName of eventsFilesFunctions) {
+  import(`../events/functions/${eventFileName}`)
+}
+console.log(`${[...eventsFilesNames, ...eventsFilesFunctions].length} Events | OK`)
