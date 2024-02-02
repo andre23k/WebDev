e34aaba56671cec460cb31230f39a90249481b87
André <102836717+andre23k@users.noreply.github.com>
2024-02-02 14:34:21 -0300

    .

--- src/events/functions/guildMemberAdd.js
+++ src/events/functions/guildMemberAdd.js
@@ -4,18 +4,19 @@ import { createRequire } from 'node:module';
 import { inviteTracker } from 'discord-inviter';
 import { InviteManager } from '../classes/inviteManager.js';
 
-const tracker = new inviteTracker(client);
 const require = createRequire(import.meta.url);
 const { e } = require("../../JSON/emojis.json");
+const tracker = new inviteTracker(client);
 const inviteManager = new InviteManager();
 
 
-tracker.on("guildMemberAdd", async (member, inviter, invite, error) => {
+tracker.on("guildMemberAdd", async (member, inviter, invite, error) => {  
+    console.log(member.user.id);
     client.events++;
     if (member.guild.id !== "1109464496164048996")
         return;
     let channel = await client.channels.fetch('1194415665503797288');
-    channel.send({
+   await channel.send({
         embeds: [{
             title: `Entrou no servidor!`,
             color: BitColors.DarkRed,
@@ -24,6 +25,5 @@ tracker.on("guildMemberAdd", async (member, inviter, invite, error) => {
             thumbnail: { url: member.user.displayAvatarURL({ forceStatic: true }) || null }
         }]
     });
-    inviteManager.handleGuildMemberAdd(member, inviter, invite, error);
+  await inviteManager.handleGuildMemberAdd(member, inviter, invite, error);
 });
-
