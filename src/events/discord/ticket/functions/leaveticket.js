497d0c1416d9d447388c3d3e546167ca9e4e69f4
André <102836717+andre23k@users.noreply.github.com>
2024-08-16 21:04:52 -0300

    .

--- src/events/discord/ticket/functions/leaveticket.js
+++ src/events/discord/ticket/functions/leaveticket.js
@@ -11,45 +11,6 @@ export async function leaveTicket(interaction) {
             ephemeral
         })
     }
-    // const modalinteraction = {
-    //     title: '',
-    //     custom_id: '',
-    //     components: [
-    //         {
-    //             type: 1,
-    //             components: [
-    //                 {
-    //                     type: 4,
-    //                     custom_id: "",
-    //                     label: "",
-    //                     style: 1,
-    //                     min_length: 0,
-    //                     max_length: 0,
-    //                     placeholder: "",
-    //                     value: '',
-    //                     required: false
-    //                 }
-    //             ]
-    //         },
-    //         {
-    //             type: 1,
-    //             components: [
-    //                 {
-    //                     type: 4,
-    //                     custom_id: "",
-    //                     label: '',
-    //                     style: 1,
-    //                     min_length: 0,
-    //                     max_length: 0,
-    //                     placeholder: '',
-    //                     value: "",
-    //                     required: true
-    //                 }
-    //             ]
-    //         }, // MAX: 5 Fields
-    //     ]
-    // };
-    // await interaction.showModal(modalinteraction)
     await interaction.reply({ content: `${e.Ok} | Ticket closed successfully.`, ephemeral: false })
     await interaction.channel.setTopic('').catch(() => null)
     await interaction.channel.permissionOverwrites.edit(interaction.user.id, { ViewChannel: false },).catch(async err => {
